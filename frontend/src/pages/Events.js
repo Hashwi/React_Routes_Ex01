42dbfa319c0c88b5513c25035fa9468714c63291
Hashwi <119130028+Hashwi@users.noreply.github.com>
2023-08-03 19:49:40 +0200

    Adding differ

--- frontend/src/pages/Events.js
+++ frontend/src/pages/Events.js
@@ -1,25 +1,23 @@
-import React from "react";
-import { json, useLoaderData } from "react-router-dom";
+import React, { Suspense } from "react";
+import { Await, defer, json, useLoaderData } from "react-router-dom";
 
 import EventsList from "../components/EventsList";
 
 function EventsPage() {
-  const data = useLoaderData();
+  const { events } = useLoaderData();
 
-  // if (data.isError){
-  //   return<p>{data.message}</p>
-  // }
-  const events = data.events;
   return (
-    <>
-      <EventsList events={events} />
-    </>
+    <Suspense fallback={<p style={{textAlign:'center'}}>Loading...</p>}>
+      <Await resolve={events}>
+        {(loadedevents) => <EventsList events={loadedevents} />}
+      </Await>
+    </Suspense>
   );
 }
 
 export default EventsPage;
 
-export async function loader() {
+async function loadEvents() {
   const response = await fetch("http://localhost:8080/events");
 
   if (!response.ok) {
@@ -27,14 +25,15 @@ export async function loader() {
     // throw new Response(JSON.stringify({message: 'Could Not Fetch Events.'}),
     // {status: 500})
 
-    throw json(
-      {message: "Could Not Fetch Events."},
-      { status: 500}
-    )
-
-
-
+    throw json({ message: "Could Not Fetch Events." }, { status: 500 });
   } else {
-    return response;
+    const resData = await response.json();
+    return resData.events
   }
+}
+
+export function loader() {
+  return defer({
+    events: loadEvents(),
+  });
 }
